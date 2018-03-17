3b481afbab2475010a94093bb0214bff39150483
wmmmm <1229819460@qq.com>
2018-03-17 14:51:38 +0800

    打包exe成功

---
+++ src/Program.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		System.in.read();
+	}
+
+}
