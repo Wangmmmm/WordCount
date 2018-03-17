eb056482103ae0d65f0d05b55805bc8d61d6575b
wmmmm <1229819460@qq.com>
2018-03-17 15:13:18 +0800

    对程序启动入口参数进行分类

--- src/Program.java
+++ src/Program.java
@@ -3,9 +3,41 @@ import java.io.IOException;
 public class Program {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.in.read();
+	    String outputFile="result.txt";
+	    if (args.length==0)
+	    	{
+	    	System.out.println("输入参数不合法");return;
+	    	}
+	    else  if(args.length==1)
+	    {
+	    	if(args[0].equals("-s"))
+	    	{
+	    		System.out.println("-s");
+	    		
+	    		return;
+	    	}
+	    	else 
+	    	{
+	    		System.out.println("输入参数不合法");return;
+	    	}
+	    }
+	    else if(args.length==2)
+	    {
+	    	String parameter=args[0];
+	  	    String input_file_name=args[1];
+	  	    System.out.println(parameter);
+	  	    System.out.println(input_file_name);
+	  	    return;
+	    }
+	  
+	    else 
+	    {
+	    	
+	    	System.out.println("输入参数不合法");
+	    	return;
+	    }
+	    
+
 	}
 
 }
